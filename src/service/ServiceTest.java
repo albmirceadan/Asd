03a5d1dbe8d4098199d748267fee6785d835098a
Dan <mirceadanalb@gmail.com>
2019-04-09 20:17:21 +0300

    com=4

--- src/service/ServiceTest.java
+++ src/service/ServiceTest.java
@@ -15,7 +15,7 @@ import validation.Validator;
 public class ServiceTest
 {
     @Test
-    public void addStudentTest()
+    public void addStudentTest0()
     {
         int count = 0;
         int countafter = 0;
@@ -33,11 +33,143 @@ public class ServiceTest
         {
             count += 1;
         }
-        service.saveStudent("20", "Calin", 932);
+        service.saveStudent(Integer.toString(count+1000), "Calin", 500);
         for (Student student : service.findAllStudents())
         {
             countafter += 1;
         }
         assert(count + 1 == countafter);
+    }
+
+    @Test
+    public void addStudentTest1()
+    {
+        int count = 0;
+        int countafter = 0;
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        for (Student student : service.findAllStudents())
+        {
+            count += 1;
+        }
+        service.saveStudent(Integer.toString(count + 10000), "Calin", 500);
+        for (Student student : service.findAllStudents())
+        {
+            countafter += 1;
+        }
+        assert(count + 1 == countafter);
+    }
+
+    @Test
+    public void addStudentTest2()
+    {
+        String group = "abc";
+        int count = 0;
+        int countafter = 0;
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        for (Student student : service.findAllStudents())
+        {
+            count += 1;
+        }
+        try
+        {
+            service.saveStudent("abc", "Calin", Integer.parseInt(group));
+        }
+        catch (NumberFormatException exc)
+        {
+
+        }
+        for (Student student : service.findAllStudents())
+        {
+            countafter += 1;
+        }
+        assert(count + 1 != countafter);
+    }
+    @Test
+    public void addStudentTest3()
+    {
+        String group = "abc";
+        int count = 0;
+        int countafter = 0;
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        for (Student student : service.findAllStudents())
+        {
+            count += 1;
+        }
+        try
+        {
+            service.saveStudent("abc", "Calin", Integer.parseInt(group));
+        }
+        catch (NumberFormatException exc)
+        {
+
+        }
+        for (Student student : service.findAllStudents())
+        {
+            countafter += 1;
+        }
+        assert(count + 1 != countafter);
+    }
+
+    @Test
+    public void addStudentTestDelete()
+    {
+        int count = 0;
+        int countafter = 0;
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        for (Student student : service.findAllStudents())
+        {
+            count += 1;
+        }
+        service.saveStudent(Integer.toString(count + 100), "s", 120);
+        System.out.println(count);
+        for (Student student : service.findAllStudents())
+        {
+            countafter += 1;
+        }
+        assert(count + 1 == countafter);
+
+        service.deleteStudent(Integer.toString(count + 100));
+        countafter = 0;
+        for (Student student : service.findAllStudents())
+        {
+            countafter += 1;
+        }
+        assert(count == countafter);
     }
 }
